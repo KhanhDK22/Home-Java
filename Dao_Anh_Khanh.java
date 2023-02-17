c90859734de836dd4cc86440f1e3c2f0be2a6313
Đào Anh Khánh <kdao2003@gmail.com>
2023-02-17 11:43:13 +0700

    bai 1

---
+++ Dao_Anh_Khanh.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Circle {
+   public double display1() {
+    return System.out.println("Dao Anh Khanh hoc Java");
+   }
+   public double display2() {
+    return System.out.print("Dao Anh Khanh ma sinh vien 2121050779");
+   }
+}
+public class Dao_Anh_Khanh {
+    public static void main(String[] args) {
+        System.out.println(c.display1());
+        System.out.println(c.display2());
+    }
+}
